770edb437b7407e0d27e1fc005bb2eec6f018248
Stefan Mermans <stefan_mermans@hotmail.com>
2016-08-31 12:13:10 +0200

    I added a small C# program to send a few command to the bike

---
+++ CS_Application_Mermans_Stefan/RemoteHealthCareCS/RemoteHealthCareCS/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteHealthCareCS
+{
+    public partial class Form1 : Form
+    {
+        public static string COMMAND_RESET = "RS\r\n";
+        public static string COMMAND_GET_ID = "ID\r\n";
+        public static string COMMAND_GET_VER = "VE\r\n";
+        public static string COMMAND_COMMAND_MODE = "CM\r\n";
+        public static string COMMAND_STATUS = "ST\r\n";
+
+        private string[] _portStrings;
+        private SerialPort _serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _serialPort = null;
+            _portStrings = SerialPort.GetPortNames();
+
+            for (int i = 0; i < _portStrings.Length; i++)
+            {
+                comboBox1.Items.Add(_portStrings[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.WriteLine(COMMAND_RESET);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _serialPort = new SerialPort((string) comboBox1.SelectedItem);
+                _serialPort.Open();
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine("Connection failed");
+                // TODO do something with this exception
+            }
+        }
+
+        private void IDButton_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.WriteLine(COMMAND_GET_ID);
+            }
+        }
+
+        private void versionButton_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.WriteLine(COMMAND_GET_VER);
+            }
+        }
+
+        private void commandMButton_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.WriteLine(COMMAND_COMMAND_MODE);
+            }
+        }
+
+        private void statusButton_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                _serialPort.WriteLine(COMMAND_STATUS);
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            if (_serialPort != null && _serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
+        }
+    }
+}

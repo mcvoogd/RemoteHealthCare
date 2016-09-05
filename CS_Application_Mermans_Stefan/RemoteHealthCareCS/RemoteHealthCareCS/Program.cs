7191506e0eb27d59475491656dbfeec916e5bdab
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-09-05 13:31:34 +0200

    Revert "Yolo"
    This reverts commit a1e94f1f843e5adf7d0b40f1e53d0410f53210b9.

--- CS_Application_Mermans_Stefan/RemoteHealthCareCS/RemoteHealthCareCS/Program.cs
+++ CS_Application_Mermans_Stefan/RemoteHealthCareCS/RemoteHealthCareCS/Program.cs
@@ -17,7 +17,6 @@ namespace RemoteHealthCareCS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-      
         }
     }
 }

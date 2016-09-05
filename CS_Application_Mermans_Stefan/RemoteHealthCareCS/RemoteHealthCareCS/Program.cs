a1e94f1f843e5adf7d0b40f1e53d0410f53210b9
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2016-09-05 13:29:20 +0200

    Yolo

--- CS_Application_Mermans_Stefan/RemoteHealthCareCS/RemoteHealthCareCS/Program.cs
+++ CS_Application_Mermans_Stefan/RemoteHealthCareCS/RemoteHealthCareCS/Program.cs
@@ -17,6 +17,7 @@ namespace RemoteHealthCareCS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+      
         }
     }
 }

b649df1c935bf09bd8e4d7586a85a88b6688f45a
Cas Koopmans <cjwkoopm@avans.nl>
2016-09-07 10:45:12 +0200

    fixed Enter

--- CS_Application_Mermans_Stefan/RemoteHealthCareCS/DataScreen/Forms/LoginForm.Designer.cs
+++ CS_Application_Mermans_Stefan/RemoteHealthCareCS/DataScreen/Forms/LoginForm.Designer.cs
@@ -125,6 +125,7 @@ namespace DataScreen.Forms
             // 
             // LoginForm
             // 
+            this.AcceptButton = this.loginButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(379, 321);

7f22c466f4ab680d6819f94f16c10b810e65d65f
Kevintjeb <Kevin.vandenbroek@home.nl>
2016-09-14 15:55:12 +0200

    finished panel

--- CS_Application_Mermans_Stefan/RemoteHealthCareCS/VRConnectorForm/VRobjects/Panel.cs
+++ CS_Application_Mermans_Stefan/RemoteHealthCareCS/VRConnectorForm/VRobjects/Panel.cs
@@ -9,11 +9,13 @@ namespace VRConnectorForm.VRobjects
 {
     class Panel
     {
-        public string Uuid;
-        public string ToSend;
+        public string Uuid { get; set; }
+        public string ToSend { get; set; }
+        public string tunnelID { get; }
 
         public Panel(string Name, int scaleValue, int x, int y, int z, int xR, int yR, int zR, int size1, int size2 ,int width, int height, int bckground1, int bckground2, int bckground3, int bckground4, string tunnelID)
         {
+            this.tunnelID = tunnelID;
             ToSend = RequestCreater.TunnelSend(new
             {
                 id = "scene/node/add",
@@ -36,6 +38,75 @@ namespace VRConnectorForm.VRobjects
                     }
                     }
 
+                }
+            }, tunnelID);
+        }
+
+        public void ClearPanel()
+        {
+            RequestCreater.TunnelSend(new
+            {
+                id = "scene/panel/clear",
+                data = new
+                {
+                    id = Uuid
+                }
+            }, tunnelID);
+        }
+
+        public void SwapPanel()
+        {
+            RequestCreater.TunnelSend(new
+            {
+                id = "scene/panel/swap",
+                data = new
+                {
+                    id = Uuid
+                }
+            }, tunnelID);
+        }
+
+        //0,0 10,10, 0,0,0,1 == x1, y1, x2,y2, r,g,b,a
+        public void DrawLines(int widthValue, int[] lineArray)
+        {
+            RequestCreater.TunnelSend(new
+            {
+                id = "scene/panel/drawlines",
+                data = new
+                {
+                    id = Uuid,
+                    width = widthValue,
+                    lines = lineArray
+                }
+            }, tunnelID);
+        }
+
+        public void SetClearColor(int[] kleur)
+        {
+            RequestCreater.TunnelSend(new
+            {
+                id = "scene/panel/setclearcolor",
+                data = new
+                {
+                    id = Uuid,
+                    color = kleur
+                }
+            }, tunnelID);
+        }
+
+        public void drawText(string textValue, int[] positie, double sizeValue, int[] kleur, string fontValue)
+        {
+            RequestCreater.TunnelSend(new
+            {
+                id = "scene/panel/setclearcolor",
+                data = new
+                {
+                    id = Uuid,
+                    text = textValue,
+                    position = positie,
+                    size = sizeValue,
+                    color = kleur,
+                    font = fontValue
                 }
             }, tunnelID);
         }

0ba3a2cb918d1d8472d28fa3e5e1b853abfddde4
MjaJong <menno.dejong@ziggo.nl>
2016-09-19 11:52:50 +0200

    temp commit to pull kevin's update

---
+++ ClientServer/ClientServer/Server/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Client
+    {
+        //Empty for now. This is needed in the BigDatabase however.
+    }
+}

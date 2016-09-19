89e459c1f9e776c1c132a0acb79a7b7681105128
Stefan Mermans <stefan_mermans@hotmail.com>
2016-09-19 13:13:17 +0200

    Working on creating a server which uses ssl to send data. Privatekey and ssl certificate are also included.

--- ClientServer/ClientServer/Server/Program.cs
+++ ClientServer/ClientServer/Server/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Server.Connection;
 
 namespace Server
 {
@@ -10,6 +12,17 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            TcpServer tcpServer = new TcpServer();
+
+            var serverThread = new Thread(tcpServer.Run);
+            serverThread.Start();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            serverThread.Interrupt();
+            serverThread.Abort();
         }
     }
 }

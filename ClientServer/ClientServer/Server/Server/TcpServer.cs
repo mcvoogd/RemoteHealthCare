838aa22ce8d65345c364cb216433f681dc439b7e
Stefan Mermans <stefan_mermans@hotmail.com>
2016-09-19 13:41:26 +0200

    fixed

--- ClientServer/ClientServer/Server/Server/TcpServer.cs
+++ ClientServer/ClientServer/Server/Server/TcpServer.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Security;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace Server.Connection
+namespace Server.Server
 {
     public class TcpServer
     {
--- ClientServer/ClientServer/Server/TinyDB/Measurement.cs
+++ ClientServer/ClientServer/Server/TinyDB/Measurement.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Server.TinyDataBase
+namespace Server.TinyDB
 {
     public class Measurement : IComparable<Measurement>
     {

710c05838a8223c97134e5aac9eb351d44779c47
Martijn <mcvoogd@avans.nl>
2016-10-10 11:31:54 +0200

    remove refresh button

--- ClientServer/ClientServer/Doctor/Forms/mainForm.cs
+++ ClientServer/ClientServer/Doctor/Forms/mainForm.cs
@@ -162,7 +162,6 @@ namespace Doctor.Forms
             label7.Font = new Font(_goodTimes, 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label8.Font = new Font(_goodTimes, 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label9.Font = new Font(_goodTimes, 18F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            refreshClientButton.Font = new Font(_goodTimes, 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
             userLabel.Font = new Font(_goodTimes, 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             userAddButton.Font = new Font(_goodTimes, 9.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             connectedLabel.Font = new Font(_goodTimes, 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);

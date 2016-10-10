85d8a34f5f84f98e78881a0bf28f9c2da885d0e3
Martijn <mcvoogd@avans.nl>
2016-10-10 13:01:56 +0200

    new fixes gui

--- ClientServer/ClientServer/Doctor/Forms/mainForm.Designer.cs
+++ ClientServer/ClientServer/Doctor/Forms/mainForm.Designer.cs
@@ -906,8 +906,7 @@ namespace Doctor.Forms
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::Doctor.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(1199, 635);
             this.Controls.Add(this.historyListBox);

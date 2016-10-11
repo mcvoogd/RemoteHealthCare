0939a46f05022520d3cc1f0b3c034164c4dc0202
Stefan Mermans <stefan_mermans@hotmail.com>
2016-10-11 18:45:00 +0200

    Fixed a small bug in the clienthandler

--- ClientServer/ClientServer/Server/Server/ClientHandler.cs
+++ ClientServer/ClientServer/Server/Server/ClientHandler.cs
@@ -149,6 +149,14 @@ namespace Server.Server
         {
             int id = data.clientId;
             var client = TcpServer.GetClientHandlerByClientID(id);
+
+            // There was a small bug: The measurement array would go out of range if the array was empty.
+            // I added a simple check to work around it.
+            if (client.Client.TinyDataBaseBase.MeasurementSystem._measurements.Count <= 0)
+            {
+                Console.WriteLine("Doctor requested patient data, but the list is empty...");
+                return;
+            }
             var measurement = client.Client.TinyDataBaseBase.MeasurementSystem._measurements
                 [client.Client.TinyDataBaseBase.MeasurementSystem._measurements.Count - 1];
             SendMessage(new
